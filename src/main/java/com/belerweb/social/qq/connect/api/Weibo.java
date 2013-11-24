c7cc662595b7a78b3a7f02a56afc0cb772d9d2c7
belerweb <belerweb@gmail.com>
2013-11-24 21:44:35 +0800

    #19, add 'qq connect get other info' api

--- src/main/java/com/belerweb/social/qq/connect/api/Weibo.java
+++ src/main/java/com/belerweb/social/qq/connect/api/Weibo.java
@@ -56,4 +56,46 @@ public final class Weibo extends API {
     return Result.parse(jsonObject.getJSONObject("data"), WeiboUser.class);
   }
 
+  /**
+   * 获取腾讯微博其他用户详细信息。
+   * 
+   * 文档地址：http://wiki.connect.qq.com/get_other_info
+   * 
+   * @param accessToken 可通过使用Authorization_Code获取Access_Token 或来获取。access_token有3个月有效期。
+   * @param openid 用户的ID，与QQ号码一一对应。
+   * @param name 其他用户的账户名。可选，name和fopenid至少选一个，若同时存在则以name值为主。
+   */
+  public Result<WeiboUser> getOtherInfo(String accessToken, String openid, String name) {
+    return getOtherInfo(connect.getClientId(), accessToken, openid, name, null);
+  }
+
+  /**
+   * 获取腾讯微博其他用户详细信息。
+   * 
+   * 文档地址：http://wiki.connect.qq.com/get_other_info
+   * 
+   * @param oAuthConsumerKey 申请QQ登录成功后，分配给应用的appid
+   * @param accessToken 可通过使用Authorization_Code获取Access_Token 或来获取。access_token有3个月有效期。
+   * @param openid 用户的ID，与QQ号码一一对应。
+   * @param name 其他用户的账户名。可选，name和fopenid至少选一个，若同时存在则以name值为主。
+   * @param fopenid 其他用户的账户名。可选，name和fopenid至少选一个，若同时存在则以name值为主。
+   */
+  public Result<WeiboUser> getOtherInfo(String oAuthConsumerKey, String accessToken, String openid,
+      String name, String fopenid) {
+    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    connect.addParameter(params, "oauth_consumer_key", oAuthConsumerKey);
+    connect.addParameter(params, "access_token", accessToken);
+    connect.addParameter(params, "openid", openid);
+    connect.addParameter(params, "format", "json");
+    connect.addNotNullParameter(params, "name", name);
+    connect.addNotNullParameter(params, "fopenid", fopenid);
+    String json = connect.get("https://graph.qq.com/user/get_other_info", params);
+    JSONObject jsonObject = new JSONObject(json);
+    Error error = Error.parse(jsonObject);
+    if (error != null) {
+      return new Result<WeiboUser>(error);
+    }
+    return Result.parse(jsonObject.getJSONObject("data"), WeiboUser.class);
+  }
+
 }
