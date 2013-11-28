94a6f4cff94263d947855c984aa42a3ce330ccb0
belerweb <belerweb@gmail.com>
2013-11-28 20:11:49 +0800

    add longitude & Latitude to address api

--- src/main/java/com/belerweb/social/SDK.java
+++ src/main/java/com/belerweb/social/SDK.java
@@ -1,11 +1,17 @@
 package com.belerweb.social;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import com.belerweb.social.bean.Error;
+import com.belerweb.social.bean.Result;
 import com.belerweb.social.exception.SocialException;
 import com.belerweb.social.http.Http;
 import com.belerweb.social.http.HttpException;
@@ -61,6 +67,33 @@ public abstract class SDK {
     if (Boolean.TRUE.equals(value)) {
       params.add(new BasicNameValuePair(name, value.toString()));
     }
+  }
+
+  /**
+   * 经纬度转换为地址
+   * 
+   * @param lon 经度
+   * @param lat 纬度
+   */
+  public Result<String> lonLatToAddress(Double lon, Double lat) {
+    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    addParameter(params, "sensor", "false");
+    addParameter(params, "language", "zh");
+    addParameter(params, "latlng", lat + "," + lon);
+    String json = get("https://maps.googleapis.com/maps/api/geocode/json", params);
+    JSONObject jsonObject = new JSONObject(json);
+    if (!"OK".equals(jsonObject.getString("status"))) {
+      Error error = new Error();
+      error.setErrorCode(jsonObject.getString("status"));
+      error.setError(jsonObject.optString("error_message"));
+      return new Result<String>(error);
+    }
+
+    JSONArray results = jsonObject.getJSONArray("results");
+    if (results.length() == 0) {
+      return new Result<String>(StringUtils.EMPTY);
+    }
+    return new Result<String>(results.getJSONObject(0).getString("formatted_address"));
   }
 
 }
